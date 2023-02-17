d972af9274636c99e8f04646d9019940a82aaf05
Vladyslav Mazurenko <mazur097mazur@gmail.com>
2023-02-17 12:04:31 +0100

    Render uploaded json as string

--- packages/dapp/src/pages/MainPage.tsx
+++ packages/dapp/src/pages/MainPage.tsx
@@ -1,3 +1,47 @@
+import { ChangeEvent, useState } from "react"
+
 export const MainPage = () => {
-  return <p>MainPage</p>
+  const [fileAsString, setFileAsString] = useState<string | null>()
+
+  const reader = new FileReader()
+  reader.onload = logFile
+
+  const handleFileSelected = (event: ChangeEvent<HTMLInputElement>): void => {
+    if (event.target.files) {
+      const files = Array.from(event.target.files)
+      reader.readAsText(files[0])
+    }
+  }
+
+  function logFile(event: ProgressEvent<FileReader>) {
+    let str = event.target?.result as string
+    if (str) {
+      setFileAsString(str?.toString())
+
+      console.log(JSON.parse(str))
+    }
+  }
+
+  return (
+    <>
+      <p>
+        Upload your contract (e.g. 'deployments.json')
+      </p>
+      <input
+        type="file"
+        accept="application/json"
+        onChange={handleFileSelected}
+      />
+
+      {(fileAsString) && (
+        <div style={{ textAlign: "left" }}>
+          <p>Your JSON result:</p>
+          <p>As string</p>
+          <pre>
+            {fileAsString?.toString()}
+          </pre>
+        </div>
+      )}
+    </>
+  )
 }
