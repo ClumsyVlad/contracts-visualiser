f772bec90a5b3fb2c320c9e3a54a686f619abbe2
Vladyslav Mazurenko <mazur097mazur@gmail.com>
2023-02-17 14:29:02 +0100

    Render contract table somehow

--- packages/dapp/src/pages/MainPage.tsx
+++ packages/dapp/src/pages/MainPage.tsx
@@ -1,8 +1,10 @@
+import React, { Fragment, useMemo } from "react"
 import { ChangeEvent, useState } from "react"
+import { ContractTable } from "../components"
 
 export const MainPage = () => {
   const [fileAsString, setFileAsString] = useState<string | null>()
-
+  const [fileAsJSON, setFileAsJSON] = useState<object | null>()
   const reader = new FileReader()
   reader.onload = logFile
 
@@ -16,14 +18,35 @@ export const MainPage = () => {
   function logFile(event: ProgressEvent<FileReader>) {
     let str = event.target?.result as string
     if (str) {
-      setFileAsString(str?.toString())
-
-      console.log(JSON.parse(str))
+      setFileAsString(str)
+      setFileAsJSON(JSON.parse(str))
     }
   }
 
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Contract Name',
+        accessor: 'contractName',
+      },
+      {
+        Header: 'Transaction Hash',
+        accessor: 'txHash',
+      },
+      {
+        Header: 'Contract address',
+        accessor: 'address',
+      },
+      {
+        Header: 'Multisig',
+        accessor: 'multisig',
+      }
+    ],
+    []
+  )
+
   return (
-    <>
+    <div className="layoutContainer">
       <p>
         Upload your contract (e.g. 'deployments.json')
       </p>
@@ -33,15 +56,24 @@ export const MainPage = () => {
         onChange={handleFileSelected}
       />
 
-      {(fileAsString) && (
-        <div style={{ textAlign: "left" }}>
-          <p>Your JSON result:</p>
-          <p>As string</p>
-          <pre>
-            {fileAsString?.toString()}
-          </pre>
-        </div>
-      )}
-    </>
+      {fileAsJSON && Object.keys(fileAsJSON).map((chain) => {
+        const chainFromJSON = fileAsJSON[chain]
+        const tableByChainData = Object.keys(chainFromJSON).map((contract) => ({
+          contractName: <span className="table__content">{contract}</span>,
+          txHash: <span className="table__content">{chainFromJSON[contract]["txHash"]}</span>,
+          address: <span className="table__content">{chainFromJSON[contract]["address"]}</span>,
+          multisig: <span className="table__content">{chainFromJSON[contract]["multisig"].toString()}</span>,
+        }))
+
+        return (
+          <div className="chainBlock" key={chain}>
+            <h4 className="chainBlock__title">Chain: {chain}</h4>
+            <div className="chainBlock__table-wrapper">
+              <ContractTable columns={columns} data={tableByChainData} />
+            </div>
+          </div>
+        )
+      })}
+    </div>
   )
 }
