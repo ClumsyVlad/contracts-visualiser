4948a7061eb33a3bb892faee3d52c2e88c3cdb31
Vladyslav Mazurenko <mazur097mazur@gmail.com>
2023-02-19 09:07:17 +0100

    Render sections per deploy

--- packages/dapp/src/pages/MainPage.tsx
+++ packages/dapp/src/pages/MainPage.tsx
@@ -1,27 +1,21 @@
-import { useMemo } from "react"
+import { useMemo, useEffect } from "react"
 import { ChangeEvent, useState } from "react"
 import { ContractTable, JSONInput } from "../components"
 import { getExplorerLinkByAddress, getExplorerLinkByTxHash, getShortenAddress } from "../utils/"
 
+import beryllium from '../../deployments/beryllium.json'
+import boron from '../../deployments/boron.json'
+import carbon from '../../deployments/carbon.json'
+import helium from '../../deployments/helium.json'
+import lithium from '../../deployments/lithium.json'
+import nitrogen from '../../deployments/nitrogen.json'
+import ragnarok from '../../deployments/ragnarok.json'
+import tequila from '../../deployments/tequila.json'
+
+const DEPLOYMENTS_LIST: Array<any> = [beryllium, boron, carbon, helium, lithium, nitrogen, ragnarok, tequila]
+const DEPLOYMENTS_LIST_NAMES = ['beryllium', 'boron', 'carbon', 'helium', 'lithium', 'nitrogen', 'ragnarok', 'tequila']
+
 export const MainPage = () => {
-  const [fileAsJSON, setFileAsJSON] = useState<Record<string, any> | null>()
-  const reader = new FileReader()
-  reader.onload = logFile
-
-  const handleFileSelected = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.files) {
-      const files = Array.from(event.target.files)
-      reader.readAsText(files[0])
-    }
-  }
-
-  function logFile(event: ProgressEvent<FileReader>) {
-    let str = event.target?.result as string
-    if (str) {
-      setFileAsJSON(JSON.parse(str))
-    }
-  }
-
   const columns = useMemo(
     () => [
       {
@@ -46,33 +40,37 @@ export const MainPage = () => {
 
   return (
     <div className="layoutContainer">
-      <p>
-        Upload your contract (e.g. 'deployments.json')
-      </p>
-      <JSONInput onChange={handleFileSelected} />
-
-      {fileAsJSON && Object.keys(fileAsJSON).map((chain) => {
-        const chainFromJSON = fileAsJSON[chain]
-        const tableByChainData = Object.keys(chainFromJSON).map((contract) => {
-          const contractName = contract
-          const txHash = chainFromJSON[contract]["txHash"]
-          const address = chainFromJSON[contract]["address"]
-          const multisig = chainFromJSON[contract]["multisig"]
-
-          return ({
-            contractName: <span className="table__content">{contractName}</span>,
-            txHash: <a href={getExplorerLinkByTxHash(chain, txHash)} target="_blank" title="Explore transaction">{getShortenAddress(txHash)} </a>,
-            address: <a href={getExplorerLinkByAddress(chain, address)} target="_blank" title="Explore contract"> {getShortenAddress(address)}</a>,
-            multisig: <span className="table__content">{multisig ? '✅' : '❌'}</span>,
-          })
-        })
-
+      {DEPLOYMENTS_LIST_NAMES.map((deployment, index) => {
+        const deploymentData = DEPLOYMENTS_LIST[index]
+        const deploymentChainNamesList = Object.keys(deploymentData).map((chain) => chain)
         return (
-          <div className="chainBlock" key={chain}>
-            <h4 className="chainBlock__title">Chain: {chain}</h4>
-            <div className="chainBlock__table-wrapper">
-              <ContractTable columns={columns} data={tableByChainData} />
-            </div>
+          <div className="deployment" key={deployment}>
+            <h2 className="deploymentTitle">
+              {deployment}
+            </h2>
+            {deploymentChainNamesList.map((chainName, index) => {
+              const dataByChainName = deploymentData[chainName]
+              const tableByChainData = Object.keys(dataByChainName).map((contract) => {
+                const contractName = contract
+                const txHash = dataByChainName[contract]["txHash"]
+                const address = dataByChainName[contract]["address"]
+                const multisig = dataByChainName[contract]["multisig"]
+                return ({
+                  contractName: <span className="table__content">{contractName}</span>,
+                  txHash: <a href={getExplorerLinkByTxHash(chainName, txHash)} target="_blank" title="Explore transaction">{getShortenAddress(txHash)} </a>,
+                  address: <a href={getExplorerLinkByAddress(chainName, address)} target="_blank" title="Explore contract"> {getShortenAddress(address)}</a>,
+                  multisig: <span className="table__content">{multisig ? '✅' : '❌'}</span>,
+                })
+              })
+              return (
+                <div className="deploymentChain" key={index}>
+                  <h3 className="deploymentChain">{chainName}</h3>
+                  <div className="chainBlock__table-wrapper">
+                    <ContractTable columns={columns} data={tableByChainData} />
+                  </div>
+                </div>
+              )
+            })}
           </div>
         )
       })}
